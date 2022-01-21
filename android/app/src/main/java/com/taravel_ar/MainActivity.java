d527faff2d1626c92648edf8f3fc651bdfe1490e
Thiagolourenco <thiagolourencosaraiva123@gmail.com>
2022-01-21 16:09:19 -0300

    :tada:

---
+++ android/app/src/main/java/com/taravel_ar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.taravel_ar;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Taravel_AR";
+  }
+}
